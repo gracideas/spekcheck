e76d289a3226033b851721c1e2dc36b386d41dbe
gracideas <soerirukz@gmail.com>
2020-03-16 03:45:53 -0600

    worked on it using cgi

--- backend.py
+++ backend.py
@@ -1,6 +1,24 @@
-#connect to database
-import mysql.connector
+#dependencies: mysql connector, python-musicbrainzngs
+!C:\Python27\python.exe -u
 
+import cgi, os
+import cgi, cgitb #import modules for CGI handling
+print "Content-type:text/html\r\n\r\n"
+print '''ENTIRE HTML CODE for submit.html
+'''
+
+form = cgi.FieldStorage() #create instance of FieldStorage
+
+#get data from input fields
+musicbrainz_id = form.getvalue('musicbrainz_id')
+source = form.getvalue('source')
+fileitem = form['filename'] #uploaded files
+
+#form validation
+mbidCheck = len('musicbrainz_id')
+sourceCheck = source.isupper() or source.islower()
+
+import mysql.connector #run duplicate check
 mydb = mysql.connector.connect(
   host="localhost",
   user="yourusername",
@@ -8,19 +26,80 @@ mydb = mysql.connector.connect(
   database="mydatabase"
 )
 
-#check MBID against existing entries
+mycursor = mydb.cursor()
 
-#perform duplicate entry checks
+sql = "SELECT * FROM SpecMetadata WHERE MusicBrainzID = %s" #prevent sql injection
+
+mycursor.execute(sql, musicbrainz_id)
+
+existingEntry = mycursor.rowcount(mycursor.fetchall())
+
+if existingEntry > 0: #entry already exists
+
+
+
+if mbidCheck < 36:
+    message = musicbrainz_id + ' is not a valid MusicBrainz ID'
+
+if sourceCheck == False:
+    message = 'You must include a source'
+if fileitem.filename:
+    # strip leading path from file name to avoid
+    # directory traversal attacks
+    fn = os.path.basename(fileitem.filename)
+    open('/tmp/' + fn, 'wb').write(fileitem.file.read())
+
+#check MBID against existing entries in database
+
+else:
+    message = 'Entry submitted'
+
+
+import musicbrainzngs
+import re
+
+
+WIP
+
+#perform duplicate entry checks WIP
+
+#insert user input into UploadLog table
+
+#website identifier for queries
+musicbrainzngs.set_useragent(
+    "SpekCheck",
+    "Alpha 1.0",
+    "EMAIL",
+)
 
 #fetch metadata from MusicBrainz
+release_id = WEBFORM INPUT
+try:
+    result = musicbrainzngs.get_release_by_id(release_id, includes=["artist", "label", "discid"], )
+except WebServiceError as exc:
+    print("Something went wrong with the request: %s" % exc)
+else:
+    release = ["release"]
+    artist = ["artist"]
+    label = ["label"]
+    catalogNo = ["discid"]
 
-#insert fetched metadata into respective table
-
-#insert user input into upload log table
+#insert user input into UploadLog table WIP
 mycursor = mydb.cursor()
 
 sql = "INSERT INTO UploadLog (Uploader,MusicBrainzID,Source) VALUES (%s, %s)"
-val = ("WEBFORM INPUT", "WEBFORM INPUT")
+val = ("WEBFORM INPUT", "WEBFORM INPUT", "WEBFORM INPUT")
+mycursor.executemany(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
+
+#insert fetched data into Metadata table WIP
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO Metadata (COLUMN NAME, COLUMN NAME) VALUES (%s, %s)"
+val = ("WEBFORM INPUT")
 mycursor.executemany(sql, val)
 
 mydb.commit()
@@ -32,4 +111,6 @@ import os
 
 import subprocess
 os.chdir('DIRECTORY')
-subprocess.call(['for %%a in *.flac *.alac *.wav *.aac *.wma *.opus *.m4a *.ape do ffmpeg -i "%%a" -lavfi showspectrumpic=s=1920x1080 "%%~na.png"'])
+subprocess.run(['for %%a in *.flac *.alac *.wav *.aac *.wma *.opus *.m4a *.ape do ffmpeg -i "%%a" -lavfi showspectrumpic=s=1920x1080 "%%~na.png" -n'])
+
+#insert spectogram into respective table
